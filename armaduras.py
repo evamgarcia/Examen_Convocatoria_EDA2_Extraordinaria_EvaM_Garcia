96e6a7ec6d1f1a96ac5e010bb1588bec002abfe6
evamgarcia <emgarcia2911@gmail.com>
2023-01-28 10:36:35 +0100

    2

--- armaduras.py
+++ armaduras.py
@@ -5,18 +5,21 @@ class Armadura:
         print(f"Se ha creado la armadura {self.nombre} de rango {self.rango} con exito")
 
     def calificacion(self):
-        self.codigo_legion = self.nombre
-        self.id_ciherenta = self.rango // 1000
-        self.id_siglo = (self.rango //100) % 10
-        self.numero_armadura = (self.rango // 10)%10
-        self.numero_escuadra = self.rango % 10
-        return f"El código de la legión es {self.codigo_legion}, el id de la cohorte es {self.id_ciherenta}, el id del siglo es {self.id_siglo}, el número de armadura es {self.numero_armadura} y el número de escuadra es {self.numero_escuadra}"
+        codigo_legion = self.nombre
+        id_ciherenta = self.rango // 1000
+        id_siglo = (self.rango //100) % 10
+        numero_armadura = (self.rango // 10)%10
+        numero_escuadra = self.rango % 10
+        return f"El código de la legión es {codigo_legion}, el id de la cohorte es {id_ciherenta}, el id del siglo es {id_siglo}, el número de armadura es {numero_armadura} y el número de escuadra es {numero_escuadra}"
+       
 
+            
 
 armadura1 = Armadura("MK", 1234)
 armadura2 = Armadura("RT", 1435)
 armadura3 = Armadura("ET", 5436)
 armadura4 = Armadura("PE", 6737)
+
 ARMADURAS = [armadura1, armadura2, armadura3, armadura4]
 calificaciones = [armadura.calificacion() for armadura in ARMADURAS]
     
